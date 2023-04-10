a00eca9007aeaea4366b735fd4461e1647f979a3
Tim hvaal <timothy.hvaal@bluefruit.co.uk>
2023-04-10 14:08:07 +0100

    Project setup - executable built with cmake.

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "print.h"
+
+int main()
+{
+    std::string str = "hello, world";
+    print(str);
+}
---
+++ src/print.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "print.h"
+
+void print(std::string input)
+{
+    (void)input;
+    std::cout << "hello, world" << std::endl;
+}
